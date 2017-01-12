abb4d685bc945f618f81eedd86a567f3dac39690
pavelgutin <pavelgutin@gmail.com>
2017-01-11 22:47:32 -0500

    first version of the script

---
+++ mintCSVtoYNAB.ps1
@@ -0,0 +1,23 @@
+﻿param(
+[string]$filename
+)
+
+(Get-Content $filename) `
+-replace '&lt;br/&gt;', ` #make the separator more readable 
+         ' | ' `
+-replace '"Date","Description","Original Description","Amount","Transaction Type","Category","Account Name","Labels","Notes"',` #fix the header
+         '"Date","Payee","Category","Memo","Outflow","Inflow"' `
+-replace '\"(\d*.\d\d)\"\,\"(debit)\"', `  #move debit to inflow
+         '"$1",""' `
+-replace '\"(\d*.\d\d)\"\,\"(credit)\"', ` #move credit to outflow
+         '"","$1"' `
+-replace '(AMAZON MARKETPLACE SEATTLE WA \| )|(AMAZON RETAIL SEATTLE WA \| )|(AMAZON DIGITAL SEATTLE WA \| )', ` #there are not necessary for me
+         '' `
+-replace '\"(?<date>[^\"]*)\",\"(?<store>[^\"]*)\",\"(?<description>[^\"]*)\",\"(?<outflow>[^\"]*)\",\"(?<inflow>[^\"]*)\",\"(?<category>[^\"]*)\",\"(?<reference>[^\"]*)\",\"(?<labels>[^\"]*)\",\"(?<notes>[^\"]*)\"',
+         '"${date}","${description}","${category}","${reference}","${outflow}","${inflow}"' `
+-replace '&amp;', `       #fix the '&' char
+         '&' `
+-replace '&#34;', `       #fix the '"' char
+         '"' | 
+Set-Content $filename
+
